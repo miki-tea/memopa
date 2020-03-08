7ec03e7601629769710bfc691a12b9156a30d6af
miki <miki.ishii16@gmail.com>
2020-03-09 01:28:16 +0900

    first_commit

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+  <span><small>2020 ©️ memopa!</small></span>
+</footer>
---
+++ head.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $subtitle; ?> |memopa</title>
+  <link rel="stylesheet" href="dist/main.css">
+</head>
+<body>
+  
+</body>
+</html>
