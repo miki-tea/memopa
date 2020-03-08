ec037b2bb5d6c4d960285198c01b0a5a3f03d9b9
miki <miki.ishii16@gmail.com>
2020-03-09 00:55:54 +0900

    環境構築

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
+const webpackStream = require("webpack-stream");
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.config");
+const minifyImg = require("gulp-imagemin");
+const minifyImg_JPG = require("imagemin-jpeg-recompress");
+const minifyImg_PNG = require("imagemin-pngquant");
+const minifyImg_GIF = require("imagemin-gifsicle");
+
+
+gulp.task("watch", () => {
+  return (gulp.watch("src/scss/*.scss", () => {
+    return (
+      gulp.src("src/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dist"))
+    );
+  }));
+});
+
+gulp.task("minify-img", () => {
+  return (
+    gulp.src("src/img/*.+(jpg|jpeg|png|gif)")
+      .pipe(minifyImg([minifyImg_JPG(), minifyImg_PNG(), minifyImg_GIF()]))
+      .pipe(gulp.dest("dist"))
+  );
+});
+
+gulp.task("webpack", () => {
+  return webpackStream(webpackConfig, webpack)
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", gulp.series("webpack", "minify-img", "watch", function (done) {
+  done();
+}));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mode: "production",
+  entry: "./src/js/main.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
