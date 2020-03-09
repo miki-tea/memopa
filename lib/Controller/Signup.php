38a0e5a5776d593b1f5541aa1bab8049e2864d04
miki <miki.ishii16@gmail.com>
2020-03-09 23:59:21 +0900

    バリデーションとモデル操作機能の追加

--- lib/Controller/Signup.php
+++ lib/Controller/Signup.php
@@ -1,53 +1,94 @@
 <?php
-namespace memopa\Controller;
+namespace MyApp\Controller;
 
-class Signup extends \memopa\Controller {
-
+// $token = filter_input(INPUT_POST,'token');
+class Signup extends \MyApp\Controller {
+  
   public function run() {
-    if($this->isLoggedIn()){ // valid session
-      header('Location:' .SITE_URL . '/mypage.php');
-      exit;
-    }
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){  //start validation
+    $email = filter_input(INPUT_POST,'email');
+    $pass = filter_input(INPUT_POST,'pass');
+    $pass_re = filter_input(INPUT_POST,'pass_re');
+
+        debug(filter_input(INPUT_POST,'email'));
+    debug('$emailのなかみ：'.$email);
+    debug('$passのなかみ：'.$pass);
+    debug('$pass_reのなかみ：'.$pass_re);
+    // if($this->isLoggedIn()){ // valid session
+    //   header('Location:' .SITE_URL . '/memopa/mypage.php');
+    //   exit;
+    // }
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        // token
+      if(!isset($_POST['token']) || $_POST['token'] !== $_SESSION['token']){
+        echo "invalid Token!";
+        exit;
+      }
+      //start validation
       $this->_validate();
-      $this->setVal('email',$_POST['email']);
-    }
+      $this->setVal('email',$email);
 
-    if($this->hasErr()){ // Fail to validate
-      return;
-    }else{ //Success to validate
-
-      //create user
-      //redirect to mypage
-
+      if($this->hasErr()){
+        debug('バリデーションエラー');
+        return;
+      } else { //Success to validate
+        debug('ユーザーテーブルに接続します。');
+          try{ 
+            //create user
+            $userModel = new \MyApp\Model\User();
+            $userModel->create([
+              'email' => $email,
+              'pass' => $pass
+            ]);
+          }catch(\MyApp\Exception\DuplicateEmail $e) {
+            debug('DB接続でエラーが発生しました。');
+            $this->setErr('common',$e->getMessage());
+            return;
+          }
+        // redirect to mypage
+        header('location: ' . SITE_URL . '/memopa/mypage.php');
+        exit;
+      }
     }
   }
+  
+  
   protected function _validate(){
-  // All: Required
-      $this->InvalidRequired($_POST['email'],'email');
-      $this->InvalidRequired($_POST['pass'],'pass');
-      $this->InvalidRequired($_POST['pass_re'],'pass_re');
+    $email = filter_input(INPUT_POST,'email');
+    $pass = filter_input(INPUT_POST,'pass');
+    $pass_re = filter_input(INPUT_POST,'pass_re');
 
-      // Email:type
-      if(empty($this->getErr('email'))){
-        $this->InvalidEmail($_POST['email'],'email');
-      }
-      // Email:Max Length
-      if(empty($this->getErr('email'))){
-        $this->InvalidMaxLen($_POST['email'],'email');
-      }
-      // Pass:Max/Min Length
-      if(empty($this->getErr('pass'))){
-        $this->InvalidMaxLen($_POST['pass'],'pass');
-      }
-      if(empty($this->getErr('pass'))){
-        $this->InvalidMinLen($_POST['pass'],'pass');
-      }
-      // Pass: Half size str
+    // All: Required
+    $this->InvalidRequired($email,'email');
+    $this->InvalidRequired($pass,'pass');
+    $this->InvalidRequired($pass_re,'pass_re');
 
-      // Pass = Pass_re
-      if(empty($this->getErr('pass'))){
-        $this->diffVal($_POST['pass'],$_POST['pass_re'],'pass');
+    //Email:type
+    if(empty($this->getErr('email'))){
+      $this->InvalidEmail($email,'email');
+    }
+    // Email:Max Length
+    if(empty($this->getErr('email'))){
+      $this->InvalidMaxLen($email,'email');
+    }
+    // Email:Duplicate
+    // if(empty($this->getErr('email'))){
+    //   $this->emailDup($email,'email');
+    // }
+    // Pass:Max Length
+    if(empty($this->getErr('pass'))){
+      $this->InvalidMaxLen($pass,'pass');
+    }
+    //Pass: Min Length
+    if(empty($this->getErr('pass'))){
+      $this->InvalidMinLen($pass,'pass');
+    }
+    // Pass: Half size str
+    if(empty($this->getErr('pass'))){
+      $this->InvalidPass($pass, 'pass');
+    }
+    // Pass = Pass_re
+    if(empty($this->getErr('pass'))){
+        $this->diffVal($pass,$pass_re,'pass');
       }
   }
 
@@ -56,19 +97,33 @@ class Signup extends \memopa\Controller {
       $this->setErr($key,'パスワード(再)と一致しません。');
     }
   }
+//   protected function emailDup($email,$key) {
+//     try{ //create user
+//       global $email;
+//       global $pass;
+//         $userModel = new \MyApp\Model\User();
+//         $userModel->create([
+//           'email' => $email,
+//           'pass' => $pass
+//         ]);
+//       } catch (\MyApp\Exception\CommonErr $e) {
+//         $this->setErr('common',$e->getMessage());
+//         return;
+//       }
+//   }
 }
 
   // protected function postProcess(){
   //   //validation
   //   try {
   //     $this->_validate();
-  //   } catch (\memopa\Exception\InvalidEmail $e){
+  //   } catch (\MyApp\Exception\InvalidEmail $e){
   //     $this->setErr('email',$e->getMessage());
       
-  //   } catch (\memopa\Exception\InvalidPassword $e){
+  //   } catch (\MyApp\Exception\InvalidPassword $e){
   //     $this->setErr('pass',$e->getMessage());
 
-  //   } catch (\memopa\Exception\UnmatchPass $e){
+  //   } catch (\MyApp\Exception\UnmatchPass $e){
   //     $this->setErr('pass_re',$e->getMessage());
   //   }
 
@@ -85,18 +140,18 @@ class Signup extends \memopa\Controller {
 
   // private function _validate() {
   //   if(empty($_POST['email'])){
-  //     throw new \memopa\Exception\InvalidEmail('入力必須です。');
+  //     throw new \MyApp\Exception\InvalidEmail('入力必須です。');
   //   }
-  //   if(empty($_POST['pass'])){
-  //     throw new \memopa\Exception\InvalidPassword('入力必須です。');
+  //   if(empty($pass)){
+  //     throw new \MyApp\Exception\InvalidPassword('入力必須です。');
   //   }
   //   if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
-  //     throw new \memopa\Exception\InvalidEmail('Eメールが無効な形式です');
+  //     throw new \MyApp\Exception\InvalidEmail('Eメールが無効な形式です');
   //   }
-  //   if(!preg_match('/\A[a-zA-Z0-9]+\z/', $_POST['pass'])){
-  //     throw new \memopa\Exception\InvalidPassword();
+  //   if(!preg_match('/\A[a-zA-Z0-9]+\z/', $pass)){
+  //     throw new \MyApp\Exception\InvalidPassword();
   //   }
-  //   if($_POST['pass'] !== $_POST['pass_re']){
-  //     throw new \memopa\Exception\UnmatchPass();
+  //   if($pass !== $_POST['pass_re']){
+  //     throw new \MyApp\Exception\UnmatchPass();
   //   }
   // }
