e9e90e25c4ed0d4d1116efc34ff073d2e345b3c2
miki <miki.ishii16@gmail.com>
2020-03-14 01:15:49 +0900

    新規登録時のuser_idが渡せないバグフィックス

--- lib/Model/User.php
+++ lib/Model/User.php
@@ -6,16 +6,19 @@ class User extends \MyApp\Model {
   
   public function create($values){
     $stmt = $this->db->prepare('INSERT INTO users (email,pass,login_time,create_date) VALUES (:email, :pass, :login_time, :create_date)');
-    $res = $stmt->execute([
+    $stmt->execute([
       ':email' => $values['email'],
       ':pass' => password_hash($values['pass'], PASSWORD_DEFAULT),
       ':login_time' => date('Y-m-d H:i:s'),
       ':create_date' => date('Y-m-d H:i:s')
     ]);
-    $_SESSION['me'] = $res['user_id'];
+    $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
+    $user = $stmt->fetch();
+     debug('$user->user_idの中身:' . $user->user_id);
     if($res === false){
       throw new \MyApp\Exception\DuplicateEmail();
     }
+    return $user;
   }
 
   public function delete($values){
