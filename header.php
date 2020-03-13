bad169e33a1c32587af5574e5b4fa18d10578b56
miki <miki.ishii16@gmail.com>
2020-03-13 21:23:49 +0900

    ヘッダー を各ページに分散編集

--- header.php
+++ header.php
@@ -19,23 +19,9 @@
         < }; >
       </ul>
     </nav> -->
+
 <header class="header">
   <div class="header__wrap">
     <div class="header__logo logo">
       <a href="index.php" class="logo__link">memopa!</a>
     </div>
-      <!-- ナビメニュー部分 -->
-    <div class="menu">
-      <ul class="menu__list">
-        <li class="menu__item"><a class="menu__link" href="sign_up.php">会員登録</a></li>
-        <li class="menu__item"><a class="menu__link" href="login.php">ログイン</a></li>
-      </ul>
-    </div>
-  </div>
-  <!-- ハンバーガーメニュー部分 --> 
-  <div class="toggle">
-      <span class="toggle__ornament"></span>
-      <span class="toggle__ornament"></span>
-      <span class="toggle__ornament"></span>
-  </div>
-</header>
