7d6165ea947bf7f1c49603b78b8c7c6852c6940e
miki <miki.ishii16@gmail.com>
2020-03-19 01:51:07 +0900

    db接続できないエラーを解消。（dbプロパティにデータベース接続設定が詰められていなかった）

--- lib/Model.php
+++ lib/Model.php
@@ -8,23 +8,27 @@ class Model {
 
   public function __construct(){
 
-    $host = getenv('host_name'); //MySQLがインストールされてるコンピュータ
-    $dbname = getenv('db_name'); //使用するDB
-    $charset = "utf8"; //文字コード
-    $user = getenv('user_name'); //MySQLにログインするユーザー名
-    $password = getenv('pass'); //ユーザーのパスワード
-
-    $options = [
-        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION, //SQLでエラーが表示された場合、画面にエラーが出力される
-        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC, //DBから取得したデータを連想配列の形式で取得する
-        \PDO::ATTR_EMULATE_PREPARES   => false, //SQLインジェクション対策
-    ];
-
-    //DBへの接続設定
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
     try {
+      $host = getenv('host_name'); //MySQLがインストールされてるコンピュータ
+      $dbname = getenv('db_name'); //使用するDB
+      $charset = "utf8"; //文字コード
+      $user = getenv('user_name'); //MySQLにログインするユーザー名
+      $password = getenv('pass'); //ユーザーのパスワード
+      // $host = 'localhost'; //MySQLがインストールされてるコンピュータ
+      // $dbname = 'memopa'; //使用するDB
+      // $charset = "utf8"; //文字コード
+      // $user = 'dbuser'; //MySQLにログインするユーザー名
+      // $password = 'memopa'; //ユーザーのパスワード
+  
+      $options = [
+          \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION, //SQLでエラーが表示された場合、画面にエラーが出力される
+          \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC, //DBから取得したデータを連想配列の形式で取得する
+          \PDO::ATTR_EMULATE_PREPARES   => false, //SQLインジェクション対策
+      ];
+      //DBへの接続設定
+      $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
         //DBへ接続
-        $dbh = new \PDO($dsn, $user, $password, $options);
+      $this->db = new \PDO($dsn, $user, $password, $options);
     } catch (\PDOException $e) {
       echo $e->getMessage();
       exit;
